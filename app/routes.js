90cae9f59e1818f352be577d6e9ea3e81bfadb60
katie.twinn@nhsbsa.nhs.uk <katie.twinn@nhsbsa.nhs.uk>
2024-01-25 16:37:40 +0000

    Updated prototype - initial run through now working

--- app/routes.js
+++ app/routes.js
@@ -16,7 +16,7 @@ router.post('/clear-data', function (req, res) {
 // Route File Versions
 // ****************************************
 
-router.use('/v1', require('./views/v1/_routes'));
+router.use('/v1/', require('./views/v1/_routes'));
 
 
 module.exports = router;
--- app/views/v1/_routes.js
+++ app/views/v1/_routes.js
@@ -16,17 +16,23 @@ router.post('/v1/start', function (req, res) {
 
 })
 
+// Continue following entering First and Last Name
+
 router.post('/v1/step-1-name', function (req, res) {
 
     res.redirect('/v1/step-2-nhsnumber');
 
 })
 
+// Continue following entering NHS Number
+
 router.post('/v1/step-2-nhsnumber', function (req, res) {
 
     res.redirect('/v1/step-3-dob');
 
 })
+
+// Continue following entering date of birth
 
 router.post('/v1/step-3-dob', function (req, res) {
 
@@ -39,59 +45,73 @@ router.post('/v1/step-3-dob', function (req, res) {
 
 router.get('/v1/step-4-previous-address', function (req, res) {
 
-    // Get the 'postcode' data from the submitted form
-    var postcodeLookup = req.session.data['postcode']
+    var postcodeLookupPrevious = req.session.data['postcodePrevious']
 
-    // Define a 'regular expression' to validate the postcode format
-    const regex = RegExp('^([A-PR-UWYZ](([0-9](([0-9]|[A-HJKSTUW])?)?)|([A-HK-Y][0-9]([0-9]|[ABEHMNPRVWXY])?)) ?[0-9][ABD-HJLNP-UW-Z]{2})$', 'i');
+    const regex = RegExp('^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$');
 
-    // Check if 'postcodeLookup' has a value
-    if (postcodeLookup) {
+    if (postcodeLookupPrevious) {
 
-        // Check if the 'postcodeLookup' matches the specified 'regular expression'
-        if (regex.test(postcodeLookup) === true) {
+        if (regex.test(postcodeLookupPrevious) === true) {
 
-            // Make an HTTP GET request to an external API (OS UK) to retrieve address data based on the postcode
-            axios.get("https://api.os.uk/search/places/v1/postcode?postcode=" + postcodeLookup + "&key="+ process.env.POSTCODEAPIKEY)
-            .then(response => {
-                // Extract and map the addresses from the API response
-                var addresses = response.data.results.map(result => result.DPA.ADDRESS);
+            axios.get("https://api.os.uk/search/places/v1/postcode?postcode=" + postcodeLookupPrevious + "&key=" + process.env.POSTCODEAPIKEY)
+                .then(response => {
+                    var previousAddresses = response.data.results.map(result => result.DPA.ADDRESS);
 
-                // Format the addresses in title case
-                const titleCaseAddresses = addresses.map(address => {
-                    const parts = address.split(', ');
-                    const formattedParts = parts.map((part, index) => {
-                        if (index === parts.length - 1) {
-                            // Preserve postcode (SW1A 2AA) in uppercase
-                            return part.toUpperCase();
-                        }
-                        return part
-                            .split(' ')
-                            .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-                            .join(' ');
+                    const previoustitleCaseAddresses = previousAddresses.map(previousAddresses => {
+                        const previousparts = previousAddresses.split(', ');
+                        const previousformattedParts = previousparts.map((part, index) => {
+                            if (index === previousparts.length - 1) {
+                                // Preserve postcode (DL14 0DX) in uppercase
+                                return part.toUpperCase();
+                            }
+                            return part
+                                .split(' ')
+                                .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+                                .join(' ');
+                        });
+                        return previousformattedParts.join(', ');
                     });
-                    return formattedParts.join(', ');
+
+                    req.session.data['previousAddresses'] = previoustitleCaseAddresses;
+
+                    res.redirect('/v1/select-previous-address')
+                })
+                .catch(error => {
+                    console.log(error);
+                    res.redirect('/v1/no-previous-address-found')
                 });
 
-                // Store the formatted addresses in the session data
-                req.session.data['addresses'] = titleCaseAddresses;
-
-                // Redirect to the 'Select Address' page
-                res.redirect('/v1/select-previous-address')
-            })
-            .catch(error => {
-                // Redirect in case of an error
-                res.redirect('/v1/no-previous-address-found')
-            });
-
-        } else {
-            // Redirect if 'postcodeLookup' doesn't match the specified 'regular expression'
-            res.redirect('/v1/step-4-previous-address')
         }
 
     } else {
-        // Redirect if 'postcodeLookup' is empty
         res.redirect('/v1/step-4-previous-address')
+    }
+
+})
+
+router.post('/v1/previous-address', function (req, res) {
+
+    var previousAddressLine1 = req.session.data['previous-address-line-1'];
+    var previousTownOrCity = req.session.data['previous-address-level2'];
+    var previousPostcodeManual = req.session.data['previous-address-postcode'];
+
+
+    if (previousAddressLine1 && previousTownOrCity && previousPostcodeManual) {
+        res.redirect('/v1/step-1-name');
+    } else {
+        res.redirect('/v1/previous-address');
+    }
+
+})
+
+router.post('/v1/select-previous-address', function (req, res) {
+
+    var previousAddress = req.session.data['previousAddress'];
+
+    if (previousAddress) {
+        res.redirect('/v1/step-5-new-address');
+    } else {
+        res.redirect('/v1/select-previous-address');
     }
 
 })
@@ -101,75 +121,94 @@ router.get('/v1/step-4-previous-address', function (req, res) {
 router.get('/v1/step-5-new-address', function (req, res) {
 
     var postcodeLookupNew = req.session.data['postcodeNew']
-    
+
     const regex = RegExp('^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$');
-    
+
     if (postcodeLookupNew) {
-    
+
         if (regex.test(postcodeLookupNew) === true) {
-    
-            axios.get("https://api.os.uk/search/places/v1/postcode?postcode=" + postcodeLookupNew + "&key="+ process.env.POSTCODEAPIKEY)
-            .then(response => {
-                var newAddresses = response.data.results.map(result => result.DPA.ADDRESS);
 
-                const newtitleCaseAddresses = newAddresses.map(newAddresses => {
-                    const newparts = newAddresses.split(', ');
-                    const newformattedParts = newparts.map((part, index) => {
-                      if (index === newparts.length - 1) {
-                        // Preserve postcode (DL14 0DX) in uppercase
-                        return part.toUpperCase();
-                      }
-                      return part
-                        .split(' ')
-                        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-                        .join(' ');
+            axios.get("https://api.os.uk/search/places/v1/postcode?postcode=" + postcodeLookupNew + "&key=" + process.env.POSTCODEAPIKEY)
+                .then(response => {
+                    var newAddresses = response.data.results.map(result => result.DPA.ADDRESS);
+
+                    const newtitleCaseAddresses = newAddresses.map(newAddresses => {
+                        const newparts = newAddresses.split(', ');
+                        const newformattedParts = newparts.map((part, index) => {
+                            if (index === newparts.length - 1) {
+                                // Preserve postcode (DL14 0DX) in uppercase
+                                return part.toUpperCase();
+                            }
+                            return part
+                                .split(' ')
+                                .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+                                .join(' ');
+                        });
+                        return newformattedParts.join(', ');
                     });
-                    return newformattedParts.join(', ');
-                  });
-    
-                req.session.data['newAddresses'] = newtitleCaseAddresses;
 
-                res.redirect('/v1/select-new-address')
-            })
-            .catch(error => {
-                console.log(error);
-                res.redirect('/v1/no-new-address-found')
-            });
-    
+                    req.session.data['newAddresses'] = newtitleCaseAddresses;
+
+                    res.redirect('/v1/select-new-address')
+                })
+                .catch(error => {
+                    console.log(error);
+                    res.redirect('/v1/no-new-address-found')
+                });
+
         }
-    
+
     } else {
-        res.redirect('/v1/step-5--new-address')
+        res.redirect('/v1/step-5-new-address')
     }
-    
+
 })
-    
- router.post('/v1/new-address', function (req, res) {
-    
+
+router.post('/v1/new-address', function (req, res) {
+
     var newAddressLine1 = req.session.data['new-address-line-1'];
     var newTownOrCity = req.session.data['new-address-level2'];
     var newPostcodeManual = req.session.data['new-address-postcode'];
-    
-    
+
+
     if (newAddressLine1 && newTownOrCity && newPostcodeManual) {
-        res.redirect('/v1/name');
+        res.redirect('/v1/step-1-name');
     } else {
         res.redirect('/v1/new-address');
     }
-    
+
 })
-    
+
 router.post('/v1/select-new-address', function (req, res) {
-    
+
     var newAddress = req.session.data['newAddress'];
 
     if (newAddress) {
-        res.redirect('/v1/confirm-address');
+        res.redirect('/v1/confirm');
     } else {
         res.redirect('/v1/select-new-address');
     }
-    
+
 })
+
+router.post('/v1/confirm', function (req, res) {
+
+    var confirmAddress = req.session.data['confirm-address'];
+
+    if (confirmAddress == "Yes") {
+        res.redirect('/v1/step-review');
+    } else {
+        res.redirect('/v1/confirm');
+    }
+
+})
+
+// Review to Completion
+
+router.post('/v1/step-review', function (req, res) {
+    res.redirect('/v1/step-completion');
+})
+
 
 
 module.exports = router;
